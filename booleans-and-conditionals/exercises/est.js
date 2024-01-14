3a73adadd3ad87f3c10986158939c8c41cf0d673
kcpage816 <kierstendotpaige@gmail.com>
2024-01-14 17:34:56 -0600

    Conditionals Exercise

---
+++ booleans-and-conditionals/exercises/est.js
@@ -0,0 +1,12 @@
+let engineIndicatorLight = 'red blinking';
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+
+if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
+    console.log("WARNING. Not ready");
+ } else {
+    console.log("all systems go");
+ }
