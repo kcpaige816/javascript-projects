cb8bf3c3cf32810539d30ea684d5fed019c503ff
kcpage816 <kierstendotpaige@gmail.com>
2024-01-15 11:27:08 -0600

    Debugging Complete for Final Liftoff

--- errors-and-debugging/exercises/DebuggingLogicErrors5.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors5.js
@@ -1,9 +1,5 @@
-// The value of launchReady assigned in the first if/else block gets changed in the second if/else block. Dangerous waters...
-// Since the issue is with launchReady, ONE way to fix the logic error is to use a different variable to store the fuel check result. 
-// Refactor the code to do this. Verify that your change works by updating the console.log statements.
-
 let launchReady = false;
-let fuelLevel = 17000;
+let fuelLevel = 21000;
 let crewStatus = true;
 let computerStatus = 'green';
 
@@ -19,10 +15,17 @@ console.log("launchReady = ", launchReady);
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
-   launchReady = true;
+   crewReady = true;
 } else {
    console.log('WARNING: Crew or computer not ready!');
-   launchReady = false;
+   crewReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("crewReady = ", crewReady);
+
+if (launchReady) {
+   console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
+   console.log('Liftoff!');
+} else {
+   console.log('Launch scrubbed.');
+}
