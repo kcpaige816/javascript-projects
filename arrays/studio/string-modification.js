71c2d2e4959133befc9a3f99ede4ef53d9fc24ce
kcpage816 <kierstendotpaige@gmail.com>
2024-01-20 22:51:56 -0600

    for Loop practice, etc.

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,9 +1,17 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
 
+let userInput = input.question('How many characters to remove?');
+let relocatedCharsCount = Number(userInput);
+
+if(relocatedCharsCount > str.length) {
+    relocatedCharsCount = 3;
+}
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+let strCut = str.substring(0, relocatedCharsCount);
+let strEnd = str.slice(relocatedCharsCount);
+console.log(`${strEnd}${strCut}`);
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
