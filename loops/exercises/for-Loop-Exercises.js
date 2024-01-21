4d0be6d52ff9f0438889b19e209ff4976384eafb
kcpage816 <kierstendotpaige@gmail.com>
2024-01-21 16:13:15 -0600

    loop exer. part 2-2

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -1,6 +1,8 @@
 let str = 'LaunchCode'
 let arr = [1, 5, 'LC101', 'blue', 42]
+let reversed = "";
 
-for (let i=0; i < arr.length; i++) {
-  console.log(arr[i]);
+for (let i = 0; i < str.length; i++) {
+  reversed = str[i] + reversed;
 }
+console.log(reversed)
