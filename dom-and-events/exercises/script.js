e5abf71ef19ebe904da33689d7cf6a9a9b25b909
kcpage816 <kierstendotpaige@gmail.com>
2024-02-24 21:44:42 -0600

    added button listeners to change color

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,17 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
+    button.addEventListener('mouseover', () => {
+        button.style.backgroundColor = 'green';
+    });
+    button.addEventListener('mouseout', () => {
+        button.style.backgroundColor = '';
+    });
+
     button.addEventListener('click', () => {
         paragraph.innerHTML = 'Houston! We have liftoff!';
     });
     
     missionAbort.addEventListener("mouseover", () => {
         missionAbort.style.backgroundColor = 'red';
-    })
+    });
 
     missionAbort.addEventListener("mouseout", () => {
         missionAbort.style.backgroundColor = '';
-    })
+    });
 
     missionAbort.addEventListener('click', () => {
         if (confirm("Are you sure you want to abort the mission?") == true) {
